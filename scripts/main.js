acbcb33c8b55abbec11115593d99d22a80069b52
Mike Mathew <m2mathew@me.com>
2015-12-05 21:28:41 -0600

    Added parse info

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,9 @@
 'use strict';
+let React = require('react');
+let ReactDOM = require('react-dom');
+let Backbone = require('backbone');
+
+window.$ = require('jquery');
+window.jQuery = $;
+Parse.initialize("ojxAI7TJorc3fVboJ9utxlGJFh3N12NR0qr7JEhp", "oMTauokDLBsa1KvvuKuBwOQaUJAlby6bSZN7QTyM");
 
