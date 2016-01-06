25d8055bbfaf954e7d1359cd3605c5b69f859719
Mike Mathew <m2mathew@me.com>
2016-01-06 13:48:40 -0600

    added a bit of history

--- scripts/components/HistoryComponent.js
+++ scripts/components/HistoryComponent.js
@@ -8,6 +8,13 @@ module.exports = React.createClass({
 		return (
 			<div>
 				<h2>History</h2>
+
+				<div>
+					<h3>Region 3</h3>
+					<div><em>Kudos</em></div>
+					<p>November 2008 - Poteet HS</p>
+					<p>Showcase Concert - Percussive Arts Society International Convention, Austin, TX</p>
+				</div>
 			</div>
 		);
 	}
